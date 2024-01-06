5a0126319a6e0b67165f88d41aba503ef85102b1
Tookim4 <tookim4@gmail.com>
2024-01-06 10:48:46 +0300

    major deploy update

--- front-end/src/features/notesService.js
+++ front-end/src/features/notesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notes/';
+const API_URL = 'https://kratos-dungeon.onrender.com/api/notes/';
 
 // Get user goals
 const getNotes = async (token) => {
